a4434b4c903501b207d1797e1441c33672a0a53f
Sai skrithik <saiskrithik.k@gmail.com>
2026-09-04 02:34:34 +0530

    feat: initialize Spring Boot backend

---
+++ backend/src/main/java/com/bookfast/bookfast_backend/BookfastBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bookfast.bookfast_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookfastBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookfastBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bookfast/bookfast_backend/BookfastBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookfast.bookfast_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookfastBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
